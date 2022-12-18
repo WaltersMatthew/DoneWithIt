50082e60f56d669c7eaa07ac72e3dd8d94e76501
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-18 12:51:02 -0800

    create listing details screen

--- App.js
+++ App.js
@@ -2,20 +2,7 @@ import React from "react";
 import { View } from "react-native";
 
 import Card from "./app/Components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 export default function App() {
-    return (
-        <View
-            style={{
-                backgroundColor: "#f8f4f4",
-                padding: 20,
-                paddingTop: 100,
-            }}
-        >
-            <Card
-                title="Red jacket for sale"
-                subTitle="$100"
-                image={require("./app/assets/jacket.jpg")}
-            />
-        </View>
-    );
+    return <ListingDetailsScreen />;
 }
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,7 @@
 export default {
     primary: "#fc5c65",
     secondary: "#4ecdc4",
-    grey: "#666",
+    medium: "#666",
+    black: "black",
     white: "#fff",
 };
