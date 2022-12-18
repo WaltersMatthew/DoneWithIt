6a6b8152c3c00e930fe379ff5114a6558fc40844
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-18 12:31:04 -0800

    build card component

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { View } from "react-native";
-import AppButton from "./app/Components/AppButton";
-import AppText from "./app/Components/AppText";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 
+import Card from "./app/Components/Card";
 export default function App() {
-    return <WelcomeScreen />;
+    return (
+        <View
+            style={{
+                backgroundColor: "#f8f4f4",
+                padding: 20,
+                paddingTop: 100,
+            }}
+        >
+            <Card
+                title="Red jacket for sale"
+                subTitle="$100"
+                image={require("./app/assets/jacket.jpg")}
+            />
+        </View>
+    );
 }
--- app/Components/AppText/AppText.js
+++ app/Components/AppText/AppText.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
-function AppText({ children }) {
-    return <Text style={styles.text}>{children}</Text>;
+function AppText({ children, style }) {
+    return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
