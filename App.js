77c7b2cd54bbf3e330a232e924dfd4088668f3be
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-19 12:29:31 -0800

    create listingsScreen component

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { View } from "react-native";
+import ListingsScreen from "./app/Components/ListingsScreen";
 
 import AccountScreen from "./app/screens/AccountScreen";
 export default function App() {
-    return <AccountScreen />;
+    return <ListingsScreen />;
 }
