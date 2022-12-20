73873fc25fc3a455a98a9652ca5971104388757b
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-19 23:10:06 -0800

    create picker selector

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Switch } from "react-native";
+import AppPicker from "./app/Components/AppPicker";
 import AppTextInput from "./app/Components/AppTextInput";
+
 import Screen from "./app/Components/Screen";
+
+const categories = [
+    { label: "Furniture", value: 1 },
+    { label: "Clothing", value: 2 },
+    { label: "Cameras", value: 3 },
+];
+
 export default function App() {
+    const [category, setCategory] = useState(categories[0]);
     return (
         <Screen>
-            <AppTextInput placeholder="Username" icon="email" />
+            <AppPicker
+                selectedItem={category}
+                onSelectItem={(item) => setCategory(item)}
+                items={categories}
+                icon="apps"
+                placeholder="Category"
+            />
+            <AppTextInput icon="email" placeholder="Email" />
         </Screen>
     );
 }
--- app/Components/AppTextInput.js
+++ app/Components/AppTextInput.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TextInput, View, StyleSheet, Platform } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import defaultStyles from "../config/styles";
