fee83a798b9c323c05fe720ffa99ae344505b6f8
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-21 12:48:32 -0800

    implement Yup for validation

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Image, StyleSheet } from "react-native";
 import { Formik } from "formik";
+import * as Yup from "yup";
 
-import Screen from "../Components/Screen";
 import AppButton from "../Components/AppButton";
+import AppText from "../Components/AppText";
 import AppTextInput from "../Components/AppTextInput";
+import Screen from "../Components/Screen";
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"),
+    password: Yup.string().required().min(4).label("Password"),
+});
 
 function LoginScreen(props) {
     return (
@@ -16,8 +23,9 @@ function LoginScreen(props) {
             <Formik
                 initialValues={{ email: "", password: "" }}
                 onSubmit={(values) => console.log(values)}
+                validationSchema={validationSchema}
             >
-                {({ handleChange, handleSubmit }) => (
+                {({ handleChange, handleSubmit, errors }) => (
                     <>
                         <AppTextInput
                             autoCapitalize="none"
@@ -28,6 +36,9 @@ function LoginScreen(props) {
                             placeholder="email"
                             textContentType="emailAddress"
                         />
+                        <AppText style={{ color: "red" }}>
+                            {errors.email}
+                        </AppText>
                         <AppTextInput
                             autoCapitalize="none"
                             autoCorrect={false}
@@ -37,6 +48,9 @@ function LoginScreen(props) {
                             secureTextEntry={true}
                             textContentType="password"
                         />
+                        <AppText style={{ color: "red" }}>
+                            {errors.password}
+                        </AppText>
                         <AppButton title="Login" onPress={handleSubmit} />
                     </>
                 )}
