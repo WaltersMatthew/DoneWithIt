3e3818badf567006f16355abd109afd43573dfe8
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-22 01:09:54 -0800

    style categories list with colored icons instead of flatlist

--- app/Components/forms/AppFormPicker.js
+++ app/Components/forms/AppFormPicker.js
@@ -4,14 +4,23 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormPicker({ items, name, placeholder, width }) {
+function AppFormPicker({
+    items,
+    name,
+    numberOfColumns,
+    PickerItemComponent,
+    placeholder,
+    width,
+}) {
     const { errors, setFieldValue, touched, values } = useFormikContext();
 
     return (
         <>
             <AppPicker
                 items={items}
+                numberOfColumns={numberOfColumns}
                 onSelectItem={(item) => setFieldValue(name, item)}
+                PickerItemComponent={PickerItemComponent}
                 placeholder={placeholder}
                 selectedItem={values[name]}
                 width={width}
