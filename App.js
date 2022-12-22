e0a1b81955469a47acb3cddd99d095c4db089992
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-22 13:11:31 -0800

    implement ImageInputList with scrolling

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import React, { useEffect, useState } from "react";
+import { Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import Screen from "./app/Components/Screen";
+import AppButton from "./app/Components/AppButton";
+import ImageInputList from "./app/Components/ImageInputList";
 
 export default function App() {
-    return <ListingEditScreen />;
+    const [imageUris, setImageUris] = useState([]);
+
+    const handleAdd = (uri) => {
+        setImageUris([...imageUris, uri]);
+    };
+
+    const handleRemove = (uri) => {
+        setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
+    };
+
+    return (
+        <Screen>
+            <ImageInputList
+                imageUris={imageUris}
+                onAddImage={handleAdd}
+                onRemoveImage={handleRemove}
+            />
+        </Screen>
+    );
 }
